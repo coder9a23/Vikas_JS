6b6f615dc83ea043aa4a6fdaea23db25ad05367d
coder9a23 <96378962+coder9a23@users.noreply.github.com>
2023-10-07 04:32:07 +0000

    forOf

---
+++ 05_Iterations/three.js
@@ -0,0 +1,24 @@
+// for of loop
+
+const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+const greetings = "Hello World"
+// for(const i of greetings){
+//     console.log(i)
+// }
+console.log(greetings.length)
+console.log(greetings.charAt(9))
+
+
+// Map
+
+const map = new Map()
+
+map.set("IN" , "India")
+map.set("Fr","France")
+
+console.log(map)
