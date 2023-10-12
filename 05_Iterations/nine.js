d739546b5900fa1ba33eea4624f79b5687d4b171
coder9a23 <96378962+coder9a23@users.noreply.github.com>
2023-10-12 02:26:15 +0000

    reduce method

--- 05_Iterations/nine.js
+++ 05_Iterations/nine.js
@@ -1,8 +1,33 @@
 const myNum = [1, 2, 3, 4]
 
-const myTotal = myNum.reduce( function (acc, currval) {
-    console.log(`acc : ${acc} and currvall ${currval}`)
-    return acc + currval
-}, 0)
+//const myTotal = myNum.reduce( function (acc, currval) {
+    //console.log(`acc : ${acc} and currvall ${currval}`)
+    //return acc + currval
+//}, 0) // 0 means the initial value of acc and this function is working as  s = s+1;
 
+//  ruduce method on arrow function
+
+const myTotal = myNum.reduce( (acc, curr) => acc+curr ,0)
 console.log(myTotal)
+
+
+const shoppingCart = [
+    {
+        cource :'Python',
+        price : 2999
+    },
+    {
+        cource :'Java',
+        price : 999
+    },
+    {
+        cource :'Mobile Application',
+        price : 5999
+    },{
+        cource :'Full statck Web Dev',
+        price : 19999
+    },
+]
+
+const pricePay = shoppingCart.reduce( (acc, item) => acc + item.price ,0) 
+console.log(pricePay)
